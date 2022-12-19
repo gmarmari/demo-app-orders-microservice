6f45ce733ebd2b491835ff429e350185ab7403f9
gmarmari <georgemarmaris87@gmail.com>
2022-12-19 21:56:48 +0100

    Add /orders path on order api (we will need it in gateway service).

--- src/main/java/gmarmari/demo/microservices/orders/api/OrdersApi.java
+++ src/main/java/gmarmari/demo/microservices/orders/api/OrdersApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/")
+@RequestMapping("/orders")
 @Tag(name = "Order API", description = "Order management API")
 public interface OrdersApi {
 
